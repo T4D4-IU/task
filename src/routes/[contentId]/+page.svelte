8dd36bf27e29b67c69b7d6d7cfc914603cc4aabd
T4D4 <21im0116@i-u.ac.jp>
2023-01-31 13:39:31 +0900

    add compornent to article page

--- src/routes/[contentId]/+page.svelte
+++ src/routes/[contentId]/+page.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
     import type { PageData } from "./$types";
     export let data:PageData;
+    import Nav from "$lib/navigate.svelte";
+    import Menu from "$lib/Menu.svelte";
+    import Footer from "$lib/footer.svelte";
   </script>
-  
+
   <svelte:head>
     <title>data.title</title>
     <meta name="description" content="Svelte demo app" />
   </svelte:head>
+
   <section>
+    <Nav />
+    <div class="top" id="top">
+      <Menu />
+    </div>
     <h1>data.title</h1>
     {@html data.body}
+    <Footer />
   </section>
+
   <style>
   h1 {
     font-size: 30px;
